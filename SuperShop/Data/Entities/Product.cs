2ccce5431dd69066fa7d6a065d5ba6be2c2dac52
nunosalg <nunosalgueiro23@gmail.com>
2024-07-25 09:36:31 +0100

    Generic repository

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SuperShop.Data.Entities
 {
-    public class Product
+    public class Product : IEntity
     {
         //[Key]
         public int Id { get; set; }
